625c22dfe21da2d12b17d94b9e1c6a7d0047dabc
Abimanyu <abhimanyuchoudhary888@gmail.com>
2025-08-07 18:25:21 +0530

    initial setup

---
+++ build/CMakeFiles/dsa_project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dsa_project.dir/main.cpp.o"
+  "CMakeFiles/dsa_project.dir/main.cpp.o.d"
+  "dsa_project"
+  "dsa_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dsa_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
