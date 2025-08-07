e5b97b31db4342906cd8892ebf52d9587748789c
Abimanyu <abhimanyuchoudhary888@gmail.com>
2025-08-07 19:14:57 +0530

    testing

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <crow.h>
 #include <iostream>
+#include <filesystem> 
 
 int main()
 {
+    crow::mustache::set_base("templates");
     crow::SimpleApp app;
     CROW_ROUTE(app, "/")([](){
-        return "Hello world!";
+        auto page = crow::mustache::load_text("home.html");
+        return page;
     });
     app.port(18080).multithreaded().run();
 }
