7ebdea71386e309778a9206d61996405b4d94d7b
Abimanyu <abhimanyuchoudhary888@gmail.com>
2025-08-29 13:35:13 +0530

    created branches

--- include/load.hpp
+++ include/load.hpp
@@ -5,18 +5,35 @@
 #include "json.hpp"
 #include <fstream> 
 using json = nlohmann::json;
-using namespace std;
-class user_data{
+class farmer_data{
 public:
-    string name;
-    string username;
-    string email;
-    string password;
-    string products[50];
-    string orders[50];
+    std::string name;
+    std::string username;
+    std::string email;
+    std::string password;
+    std::string products[50];
+    std::string orders[50];
     bool isEmpty;
-    user_data(){isEmpty=true;};
-    user_data(string thename,string theusername,string theemail,string thepassword){
+    farmer_data(){isEmpty=true;};
+    farmer_data(std::string thename,std::string theusername,std::string theemail,std::string thepassword){
+        name=thename;
+        username=theusername;
+        email=theemail;
+        password=thepassword;
+        isEmpty=false;
+    }
+};
+
+class buyer_data{
+public:
+    std::string name;
+    std::string username;
+    std::string email;
+    std::string password;
+    std::string orders[50];
+    bool isEmpty;
+    buyer_data(){isEmpty=true;};
+    buyer_data(std::string thename,std::string theusername,std::string theemail,std::string thepassword){
         name=thename;
         username=theusername;
         email=theemail;
@@ -26,7 +43,7 @@ public:
 };
 
 //FNV-1a hashing
-uint32_t fnv1a(string s){
+uint32_t fnv1a(std::string s){
     const uint32_t bais=2166136261u;
     const uint32_t prime=16777619u;
     uint32_t hash=bais;
@@ -37,25 +54,48 @@ uint32_t fnv1a(string s){
     return hash%100;
 
 }
-user_data* user_hashtable(){
-    ifstream file("db/json/farmers.json");
+farmer_data* farmer_hashtable(){
+    std::ifstream file("db/json/farmers.json");
     if (!file.is_open()) {
         std::cerr << "Could not open the file!" << std::endl;
         return nullptr;
     }
     json data;
-    user_data* hashArr=new user_data[100];
+    farmer_data* hashArr=new farmer_data[100];
     file>>data;
     for(const auto& user:data){
         uint32_t index=fnv1a(user["username"]);
         if(hashArr[index].isEmpty){
-            hashArr[index]=user_data(user["name"],user["username"],user["email"],user["password"]);
+            hashArr[index]=farmer_data(user["name"],user["username"],user["email"],user["password"]);
         }
         else{
             while(!hashArr[index].isEmpty){
                 index=(index+1)%100;
             }
-            hashArr[index]=user_data(user["name"],user["username"],user["email"],user["password"]);
+            hashArr[index]=farmer_data(user["name"],user["username"],user["email"],user["password"]);
+        }
+    }
+    return hashArr;
+} 
+buyer_data* buyer_hashtable(){
+    std::ifstream file("db/json/buyers.json");
+    if (!file.is_open()) {
+        std::cerr << "Could not open the file!" << std::endl;
+        return nullptr;
+    }
+    json data;
+    buyer_data* hashArr=new buyer_data[100];
+    file>>data;
+    for(const auto& user:data){
+        uint32_t index=fnv1a(user["username"]);
+        if(hashArr[index].isEmpty){
+            hashArr[index]=buyer_data(user["name"],user["username"],user["email"],user["password"]);
+        }
+        else{
+            while(!hashArr[index].isEmpty){
+                index=(index+1)%100;
+            }
+            hashArr[index]=buyer_data(user["name"],user["username"],user["email"],user["password"]);
         }
     }
     return hashArr;
